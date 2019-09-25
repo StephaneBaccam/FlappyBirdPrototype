d71ddb5520072f32a4cf093fedb2f3b7adf73d1b
StephaneBaccam <34456841+StephaneBaccam@users.noreply.github.com>
2019-09-25 22:16:35 +0200

    Ajouts + Modifications
    Ajout d'un compteur de score et d'un écran game over
    Modification de Flappy

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bird : MonoBehaviour
 {
     private float jumpH = 10f;
     private Rigidbody2D rb2D;
+    public GameObject GameOverUI;
+    public Text countScore;
+    public Text ScoreTot;
+    int count;
 
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -28,7 +35,21 @@ public class Bird : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Game Over"); //Peut être modifier pour afficher un Texte.
-        Time.timeScale = 0;  //Arrête le jeu lorsqu'on entre en collision avec une pipe
+        if (collision.gameObject.CompareTag("Score"))
+        {
+            count += 1;
+            SetCountText();
+        }
+        else
+        {
+            Debug.Log("Game Over"); //Peut être modifier pour afficher un Texte.
+            Time.timeScale = 0;  //Arrête le jeu lorsqu'on entre en collision avec une pipe
+            ScoreTot.text = "Score : " + count.ToString();
+            this.GameOverUI.SetActive(true);
+        }
+    }
+        void SetCountText()
+    {
+        countScore.text = "Score : " + count.ToString();
     }
 }
